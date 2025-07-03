df23d2920f767a10d29ba3747856a2e762cb4b27
Satya Ravichandrakumar <satya.ravichandrakumar@hurixdc.local>
2025-07-03 17:18:35 +0530

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import LandingPage from './components/Landingpage/landingPage.tsx'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <LandingPage />
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/components/Landingpage/landingPage.tsx
@@ -0,0 +1,10 @@
+import { useState, useRef, type ChangeEvent, type KeyboardEvent } from "react";
+import './landingPage.css'
+export default function LandingPage() {
+
+    return(
+        <>
+        <p>hello again brother LandingPage</p>
+        </>
+    )
+}
